a83d3098dfff1fcb506afe0f927855f6b66258fa
ZVS <74719027+kitten-owner@users.noreply.github.com>
2023-12-25 23:38:56 +0700

    Add files via upload

---
+++ Lab_1.cpp
@@ -0,0 +1,52 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <iostream>
+
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int ready = 0;
+
+void* producer(void*) 
+{
+    while (true) 
+    {
+        pthread_mutex_lock(&lock);
+        if (ready)
+        {
+            pthread_mutex_unlock(&lock);
+            continue;
+        }
+        ready = 1;
+        printf("Sent\n");
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&lock);
+        sleep(1);
+    }
+    return nullptr;
+}
+
+void* consumer(void*) 
+{
+    while (true) 
+    {
+        pthread_mutex_lock(&lock);
+        while (!ready) 
+        {
+            pthread_cond_wait(&cond, &lock);
+        }
+        ready = 0;
+        printf("Received\n");
+        pthread_mutex_unlock(&lock);
+    }
+    return nullptr;
+}
+
+int main() 
+{
+    pthread_t producerThread, consumerThread;
+
+    pthread_create(&producerThread, nullptr, producer, nullptr);
+    pthread_create(&consumerThread, nullptr, consumer, nullptr);
+
+    return 0;
+}
